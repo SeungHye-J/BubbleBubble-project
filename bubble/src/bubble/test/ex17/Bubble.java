3a1717e68c54b57337b02e166260e3719e8fa83d
SeungHye-J <ivory0130@gmail.com>
2024-01-12 20:50:19 +0900

    물방울이 된 적군 터트리기

---
+++ bubble/src/bubble/test/ex17/Bubble.java
@@ -0,0 +1,202 @@
+package bubble.test.ex17;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Bubble extends JLabel implements Moveable {
+
+	// 의존성 컴포지션
+	private BubbleFrame mContext;// mtContext라는 뜻
+	private Player player;
+	private Enemy enemy;
+	private BackgroundBubbleService backgroundBubbleService;
+
+	// 위치상태
+	private int x;
+	private int y;
+
+	// 움직임의 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+
+	// 적군을 맞춘 상태
+	private int state;// 0(물방울),1(적을 가둔 물방울)
+
+	private ImageIcon bubble;// 물방울
+	private ImageIcon bubbled;// 적을 가둔 물방울
+	private ImageIcon bomb; // 물방울이 터진 상태
+
+	public Bubble(BubbleFrame mContext) {
+		// this.player = player; // player가 있어야x와y좌표를 알 수 있음.
+		this.mContext = mContext;
+		this.player = mContext.getPlayer();
+		this.enemy = mContext.getEnemy();
+		initObject();
+		initSetting();
+	}
+
+	private void initObject() {
+		bubble = new ImageIcon("image/bubble.png");
+		bubbled = new ImageIcon("image/bubbled.png");
+		bomb = new ImageIcon("image/bomb.png");
+
+		backgroundBubbleService = new BackgroundBubbleService(this);
+
+	}
+
+	private void initSetting() {
+		left = false;
+		right = false;
+		up = false;
+
+		x = player.getX();
+		y = player.getY();
+
+		setIcon(bubble);
+		setSize(50, 50);
+
+		state = 0;
+	}
+
+	@Override
+	public void left() {
+		left = true;
+		for (int i = 0; i < 400; i++) {
+			x--;
+			setLocation(x, y);
+
+			if (backgroundBubbleService.leftWall()) {
+				left = false;
+				break;
+			}
+
+			//아군과 적군의 거리가 10
+			if (Math.abs(x - enemy.getX()) < 10 && // X좌표검사
+					(Math.abs(y - enemy.getY()) > 0 && Math.abs(y - enemy.getY()) < 50))// Y좌표검사
+			{
+				// bubble의X값과 enenmy의 X값
+				if(enemy.getState() == 0) {
+					attack();
+					break;
+				}
+			}
+
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		up();
+	}
+
+	@Override
+	public void right() {
+		right = true;
+		for (int i = 0; i < 400; i++) {
+			x++;
+			setLocation(x, y);
+
+			if (backgroundBubbleService.rightWall()) {
+				right = false;
+				break;
+			}
+			
+			//아군과 적군의 거리가 10
+			if (Math.abs(x - enemy.getX()) < 10 && // X좌표검사
+					(Math.abs(y - enemy.getY()) > 0 && Math.abs(y - enemy.getY()) < 50))// Y좌표검사
+			{
+				// bubble의X값과 enenmy의 X값
+				if(enemy.getState() == 0) {
+					attack();
+					break;
+				}
+			}
+
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		up();
+	}
+
+	@Override
+	public void up() {
+		up = true;
+		while (up) {
+			y--;
+			setLocation(x, y);
+
+			if (backgroundBubbleService.topWall()) {
+				up = false;
+				break;
+			}
+
+			
+
+			try {
+				if (state == 0) {//기본 물방울
+					Thread.sleep(1);
+				}else { //적을 가둔 물방울
+					Thread.sleep(10);
+				}
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		if(state == 0) {
+			clearBubble(); // 천장에 버블이 도착하고 나서 3초 후에 메모리에서 소멸
+		}
+		
+	}
+
+	// 적군 물방울 공격
+	@Override
+	public void attack() {
+		state = 1;//물방울
+		enemy.setState(1);
+		setIcon(bubbled);// icon 변경
+		mContext.remove(enemy);// 적군 메모리에서 삭제(가비지 컬렉션-> 즉시 발동하지 않음)
+		mContext.repaint();
+	}
+
+	// 행위 -> clear (동사) -> bubble(목적어)
+	public void clearBubble() {
+		try {
+			Thread.sleep(3000);
+			setIcon(bomb);
+			Thread.sleep(500);
+			mContext.remove(this); // BubbleFrame의 bubble이 메모리에서 소멸된다.
+			mContext.repaint();// BubbleFrame의 전체를 다시그린다.(메모리에서 없는건 그리지 않음)
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//적군 맞춘 후 clear
+	public void clearBubbled() {
+		new Thread(()->{
+			System.out.println("clearBubbled");
+			try {
+				up = false;
+				setIcon(bomb);
+				Thread.sleep(1000);
+				mContext.remove(this);
+				mContext.repaint();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}).start();
+		
+	}
+
+}
---
+++ bubble/src/bubble/test/ex17/Player.java
@@ -0,0 +1,181 @@
+package bubble.test.ex17;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//calss Player -> new 가능한 애들!! 게임에 존재할 수 있음(추상메서드를 가질 수 없다)
+@Getter
+@Setter
+public class Player extends JLabel implements Moveable {
+
+	private BubbleFrame mContext;
+	private List<Bubble> bubbleList;
+	
+	//위치상태
+	private int x;
+	private int y;
+	
+	//플레이어의 방향
+	private PlayerWay playerWay;
+
+	// 움직임의 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+
+	// 벽에 충돌한 상태
+	private boolean leftWallCrash;
+	private boolean rightWallCrash;
+
+	// 플레이어 속도 상태
+	private final int SPEED = 4; // 상수대문자
+	private final int JUMPSPEED = 2; // up,down
+
+	private ImageIcon playerR, playerL;// 캐릭터가 오른쪽을볼지 왼쪽을볼지 상태
+
+	public Player(BubbleFrame mContext) {
+		this.mContext = mContext;
+
+		initObject();
+		initSetting();
+		initBackGroundPlayerService();
+	}
+
+	private void initObject() {
+		playerR = new ImageIcon("image/playerR.png");
+		playerL = new ImageIcon("image/playerL.png");
+		bubbleList = new ArrayList<>();
+	}
+
+	// 최초 상태
+	private void initSetting() {
+		x = 80;
+		y = 535;
+
+		// 최초상태는 움직임이 없음
+		left = false;
+		right = false;
+		up = false;
+		down = false;
+
+		leftWallCrash = false;
+		rightWallCrash = false;
+
+		playerWay = PlayerWay.RIGHT;
+		
+		setIcon(playerR);
+		setSize(50, 50);
+		setLocation(x, y);
+	}
+
+	private void initBackGroundPlayerService() {
+		new Thread(new BackGroundPlayerService(this)).start();
+	}
+	
+	@Override
+	public void attack() {
+		new Thread(()->{
+			Bubble bubble = new Bubble(mContext);
+			mContext.add(bubble);
+			bubbleList.add(bubble);
+			if(playerWay == playerWay.LEFT) {
+				bubble.left();
+			}else {
+				bubble.right();
+			}
+		}).start();
+	}
+
+	// 이벤트 핸들러
+	@Override
+	public void left() {
+//		System.out.println("left");
+		playerWay = PlayerWay.LEFT;
+		left = true;
+
+		new Thread(() -> {
+			while (left) {
+				setIcon(playerL);
+				x = x - SPEED;
+				setLocation(x, y);
+
+				try {
+					Thread.sleep(10); // 0.01초
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+
+	}
+
+	@Override
+	public void right() {
+//		System.out.println("right");
+		playerWay = PlayerWay.RIGHT;
+		right = true;
+
+		new Thread(() -> {
+			while (right) {
+				setIcon(playerR);
+				x = x + SPEED;
+				setLocation(x, y);
+
+				try {
+					Thread.sleep(10); // 0.01초
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+
+	}
+
+	// left + up , right + up
+	@Override
+	public void up() {
+//		System.out.println("up");
+		up = true;
+
+		new Thread(() -> {
+			for (int i = 0; i < 130 / JUMPSPEED; i++) {
+				y = y - JUMPSPEED; // 왼쪽상단 좌표가 0,0이기때문에 up은 -(minus)해줘야함
+				setLocation(x, y);
+				try {
+					Thread.sleep(5);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+			up = false;
+			down();
+		}).start();
+	}
+
+	@Override
+	public void down() {
+//		System.out.println("down");
+		down = true;
+
+		new Thread(() -> {
+			while (down) {
+				y = y + JUMPSPEED;
+				setLocation(x, y);
+				try {
+					Thread.sleep(3);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			down = false;
+		}).start();
+	}
+}
